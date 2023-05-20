2243a631c0030b4daa7470291ef5fc5c08910705
tuke <jrriwll@gmail.com>
2023-05-20 12:53:21 +0800

    commit

---
+++ marco/proc/src/builder.rs
@@ -0,0 +1,57 @@
+use proc_macro2::{TokenStream};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput, Fields, Data};
+use crate::map_fields;
+
+
+#[proc_macro_derive(Builder)]
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    // let ident_builder = Ident::new(&format!("{}Builder", ident), ident.span());
+    let ident_builder = format_ident!("{}Builder", ident);
+    if let Data::Struct(r#struct) = input.data {
+        let fields = r#struct.fields;
+        if matches!(&fields, Fields::Named(_)) {
+            let builder_fields = map_fields(&fields, |(ident, ty)| quote!(#ident: Option<#ty>, ));
+            let builder_set_fields = map_fields(&fields, |(ident, ty)| {
+                quote!(pub fn #ident(mut self, value: #ty) -> Self {
+                    self.#ident = Some(value);
+                    self
+                })
+            });
+            let build_lets = map_fields(&fields, |(ident, _)| {
+                quote!(
+                    let #ident = self.#ident.ok_or(format!(
+                        "field \"{}\" required, but not set yet.",
+                        stringify!(#ident),
+                    ))?;
+                )
+            });
+            let build_values = map_fields(&fields, |(ident, _)| quote!(#ident,));
+            let tokens = quote!(
+                impl #ident {
+                    pub fn builder() -> #ident_builder {
+                        #ident_builder::default()
+                    }
+                }
+
+                #[derive(Debug, Default)]
+                pub struct #ident_builder {
+                    #builder_fields
+                }
+
+                impl #ident_builder {
+                    #builder_set_fields
+
+                    pub fn build(self) -> Result<#ident, String> {
+                        #build_lets
+                        Ok(#ident { #build_values })
+                    }
+                }
+            );
+            return proc_macro::TokenStream::from(tokens);
+        }
+    }
+    proc_macro::TokenStream::from(quote!())
+}
